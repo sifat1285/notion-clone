3a7a7a5e15a36fb5763f89a6fe735896e4657b3b
sifat1285 <m.sifat1666690@gmail.com>
2023-12-18 22:42:39 +0600

    deploy

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://actual-glider-21.clerk.accounts.dev",
+      domain: "https://perfect-silkworm-81.clerk.accounts.dev",
       applicationID: "convex",
     }
   ]
